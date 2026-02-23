9ba8fa6ba7c5b36b2b2b66789f96b99dc0db9dab
Paul Kotsan <paul@Pavlos-MacBook-Pro.local>
2026-02-23 15:52:40 -0500

    Images loading fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/react_phone-catalog/',
   plugins: [react()],
   css: {
     preprocessorOptions: {
       scss: {
-        loadPaths: ['./react_phone-catalog'],
+        loadPaths: ['./src'],
       },
     },
   },
